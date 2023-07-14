14850e3c5d0ec435bdae7877190f290746c597a7
verstands <rabbymbamu@gmail.com>
2023-07-14 12:49:41 +0100

    Reducer type_depense

--- src/Reducers/TypeDepenseReducer.js
+++ src/Reducers/TypeDepenseReducer.js
@@ -1,4 +1,8 @@
-import { GET_TYPE_DEPENSE, ADD_TYPE_DEPENSE } from "../actions/TypeDepenseAction"
+import {
+    GET_TYPE_DEPENSE,
+    ADD_TYPE_DEPENSE,
+    DELETE_TYPE_DEPENSE
+} from "../actions/TypeDepenseAction"
 
 
 const initiatState = {}
@@ -9,6 +13,8 @@ export default function TypeDepenseReducer(state = initiatState, action) {
             return action.payload
         case ADD_TYPE_DEPENSE:
             return [action.payload, ...state]
+        case DELETE_TYPE_DEPENSE:
+            return state.filter((postMembre) => postMembre.id != action.payload);
         default:
             return state
     }
