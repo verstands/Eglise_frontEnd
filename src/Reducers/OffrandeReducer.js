87d5fe4d31b4eab56d9f2fbe682816bc1af2025f
verstands <rabbymbamu@gmail.com>
2023-07-14 12:47:41 +0100

    Reducer offrande

--- src/Reducers/OffrandeReducer.js
+++ src/Reducers/OffrandeReducer.js
@@ -1,12 +1,14 @@
-import { GET_OFFRANDE } from "../actions/OffrandeAction"
+import { GET_OFFRANDE, DELETE_OFFRANDE } from "../actions/OffrandeAction"
 
 
 const initiatState = {}
 
-export default function offrandeReducer(state = initiatState, action){
-   switch(action.type){
+export default function offrandeReducer(state = initiatState, action) {
+   switch (action.type) {
       case GET_OFFRANDE:
          return action.payload
+      case DELETE_OFFRANDE:
+         return state.filter((postMembre) => postMembre.id != action.payload);
       default:
          return state
    }
