9039b4384e3509cd3321462f16754aacb77e5cac
verstands <rabbymbamu@gmail.com>
2023-07-14 12:50:24 +0100

    Reducer enfant

--- src/Reducers/Enfant.reducer.js
+++ src/Reducers/Enfant.reducer.js
@@ -7,7 +7,7 @@ export default function enfantReducer(state = initiatState, action) {
    switch (action.type) {
       case GET_MEMBRE_ENFANT:
          return action.payload
-      case DELETE_DEVISE:
+      case DELETE_MEMBRE_ENFANT:
          return state.filter((post) => post.id != action.payload);
       default:
          return state
