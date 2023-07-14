c38d3aadc1886f68dfa992d082a0a716bab667bc
verstands <rabbymbamu@gmail.com>
2023-07-14 12:28:50 +0100

    Reducer culte

--- src/Reducers/CulteReducer.js
+++ src/Reducers/CulteReducer.js
@@ -1,4 +1,4 @@
-import { GET_CULTE } from "../actions/CulteAction"
+import { GET_CULTE, DELETE_CULTE } from "../actions/CulteAction"
 
 
 const initiatState = {}
@@ -7,6 +7,8 @@ export default function culteReducer(state = initiatState, action){
    switch(action.type){
       case GET_CULTE:
          return action.payload
+         case DELETE_CULTE:
+         return state.filter((post) => post.id != action.payload);
       default:
          return state
    }
