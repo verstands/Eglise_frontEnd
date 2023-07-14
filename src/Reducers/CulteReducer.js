68916c7c0daa7cfb659d4405c4ecd432677743fc
verstands <rabbymbamu@gmail.com>
2023-07-14 12:30:27 +0100

    Reducer DEPARTEMENT

--- src/Reducers/CulteReducer.js
+++ src/Reducers/CulteReducer.js
@@ -3,11 +3,11 @@ import { GET_CULTE, DELETE_CULTE } from "../actions/CulteAction"
 
 const initiatState = {}
 
-export default function culteReducer(state = initiatState, action){
-   switch(action.type){
+export default function culteReducer(state = initiatState, action) {
+   switch (action.type) {
       case GET_CULTE:
          return action.payload
-         case DELETE_CULTE:
+      case DELETE_CULTE:
          return state.filter((post) => post.id != action.payload);
       default:
          return state
--- src/Reducers/Departement.reducer.js
+++ src/Reducers/Departement.reducer.js
@@ -1,12 +1,14 @@
-import { GET_DEPARTEMENT } from "../actions/Departemant.action";
+import { GET_DEPARTEMENT, DELETE_DEPARTEMENT } from "../actions/Departemant.action";
 
 
 const initiatState = {}
 
-export default function  deparmentReduceur(state = initiatState, action){
-   switch(action.type){
+export default function deparmentReduceur(state = initiatState, action) {
+   switch (action.type) {
       case GET_DEPARTEMENT:
          return action.payload
+      case DELETE_DEPARTEMENT:
+         return state.filter((post) => post.id != action.payload);
       default:
          return state
    }
