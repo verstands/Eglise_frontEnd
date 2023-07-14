c9f12364cbd64ee385681c689e5540615461da6b
verstands <rabbymbamu@gmail.com>
2023-07-14 10:55:14 +0100

    delete action devise

--- src/actions/DeviseAction.js
+++ src/actions/DeviseAction.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import Token from "../loadingErr/Token";
 
 export const GET_DEVISE = "GET_DEVISE";
+export const DELETE_DEVISE = "DELETE_DEVISE";
 const url = "http://localhost:5000/api/";
 
 
@@ -20,3 +21,37 @@ export const getDevise = () => {
             })
     }
 }
+
+export const deleteDevise = (id) => {
+    return (dispatch) => {
+        Swal.fire({
+            title: 'Êtes-vous sûr de vouloir supprimer ce Categorie ?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Oui, supprimer',
+            cancelButtonText: 'Non, annuler'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios
+                    .delete(`${url}devise/${id}`, {
+                        headers: {
+                            Accept: 'application/json',
+                            'Content-Type': 'application/json',
+                            Authorization: Token()
+                        }
+                    })
+                    .then((response) => {
+                        dispatch({ type: DELETE_DEVISE, payload: id });
+                        Swal.fire({
+                            icon: 'success',
+                            text: `${response.data.message}`,
+                            confirmButtonText: 'OK'
+                        });
+                    })
+                    .catch((error) => {
+                        alert(error);
+                    });
+            }
+        });
+    };
+};
