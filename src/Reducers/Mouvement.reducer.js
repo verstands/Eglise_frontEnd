13dc5aa85ef0f14feaab1991fdc662075f013f84
verstands <rabbymbamu@gmail.com>
2023-07-14 12:46:33 +0100

    Reducer nouveau

--- src/Reducers/Mouvement.reducer.js
+++ src/Reducers/Mouvement.reducer.js
@@ -2,12 +2,12 @@ import { GET_MOUVEMENT, DELETE_MOUVEMENT } from "../actions/Mouvement.action"
 
 const initiatState = {}
 
-export default function  mouvementReducer(state = initiatState, action){
-   switch(action.type){
+export default function mouvementReducer(state = initiatState, action) {
+   switch (action.type) {
       case GET_MOUVEMENT:
          return action.payload
-         case DELETE_MOUVEMENT:
-            return state.filter((postMembre) => postMembre.id != action.payload);
+      case DELETE_MOUVEMENT:
+         return state.filter((postMembre) => postMembre.id != action.payload);
       default:
          return state
    }
--- src/Reducers/Nouveau.reducer.js
+++ src/Reducers/Nouveau.reducer.js
@@ -1,11 +1,13 @@
-import { GET_NOUVEAUNE } from "../actions/Nouveau.action"
+import { GET_NOUVEAUNE, DELETE_NOUVEAU } from "../actions/Nouveau.action"
 
 const initiatState = {}
 
-export default function  nouveauneReducer(state = initiatState, action){
-   switch(action.type){
+export default function nouveauneReducer(state = initiatState, action) {
+   switch (action.type) {
       case GET_NOUVEAUNE:
          return action.payload
+      case DELETE_NOUVEAU:
+         return state.filter((postMembre) => postMembre.id != action.payload);
       default:
          return state
    }
