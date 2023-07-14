ee0f70b3de33f20878c546e2bed6a15abd23dfda
verstands <rabbymbamu@gmail.com>
2023-07-14 10:53:41 +0100

    delete action depense

--- src/actions/DepenseAction.js
+++ src/actions/DepenseAction.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import Token from "../loadingErr/Token";
 
 export const GET_DEPENSE = "GET_DEPENSE";
+export const DELETE_DEPENSE = "DELETE_DEPENSE";
 const url = "http://localhost:5000/api/";
 
 
@@ -20,3 +21,37 @@ export const getDepense = () => {
             })
     }
 }
+
+export const deleteDepense = (id) => {
+    return (dispatch) => {
+        Swal.fire({
+            title: 'Êtes-vous sûr de vouloir supprimer ce Categorie ?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Oui, supprimer',
+            cancelButtonText: 'Non, annuler'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios
+                    .delete(`${url}depense/${id}`, {
+                        headers: {
+                            Accept: 'application/json',
+                            'Content-Type': 'application/json',
+                            Authorization: Token()
+                        }
+                    })
+                    .then((response) => {
+                        dispatch({ type: DELETE_DEPENSE, payload: id });
+                        Swal.fire({
+                            icon: 'success',
+                            text: `${response.data.message}`,
+                            confirmButtonText: 'OK'
+                        });
+                    })
+                    .catch((error) => {
+                        alert(error);
+                    });
+            }
+        });
+    };
+};
