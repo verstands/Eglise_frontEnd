dfd6bdc32abda799f59eab8f9abe81df9134a0e7
verstands <rabbymbamu@gmail.com>
2023-07-14 10:59:23 +0100

    delete action materiel

--- src/actions/MaterielAction.js
+++ src/actions/MaterielAction.js
@@ -62,3 +62,37 @@ export const addMateriel = (postData) => {
             })
     }
 }
+
+export const deleteMateriel = (id) => {
+    return (dispatch) => {
+        Swal.fire({
+            title: 'Êtes-vous sûr de vouloir supprimer ce Categorie ?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Oui, supprimer',
+            cancelButtonText: 'Non, annuler'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios
+                    .delete(`${url}materiel/${id}`, {
+                        headers: {
+                            Accept: 'application/json',
+                            'Content-Type': 'application/json',
+                            Authorization: Token()
+                        }
+                    })
+                    .then((response) => {
+                        dispatch({ type: DELETE_MATERIEL, payload: id });
+                        Swal.fire({
+                            icon: 'success',
+                            text: `${response.data.message}`,
+                            confirmButtonText: 'OK'
+                        });
+                    })
+                    .catch((error) => {
+                        alert(error);
+                    });
+            }
+        });
+    };
+};
