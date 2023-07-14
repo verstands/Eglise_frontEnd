fb3637fd7b51d080bb5aacc307b514a7314282ed
verstands <rabbymbamu@gmail.com>
2023-07-14 11:07:20 +0100

    delete action offrande

--- src/actions/OffrandeAction.js
+++ src/actions/OffrandeAction.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import Token from "../loadingErr/Token";
 
 export const GET_OFFRANDE = "GET_OFFRANDE";
+export const DELETE_OFFRANDE = "DELETE_OFFRANDE";
 const url = "http://localhost:5000/api/";
 
 
@@ -20,3 +21,37 @@ export const getOffrande = () => {
             })
     }
 }
+
+export const deleteOffrande = (id) => {
+    return (dispatch) => {
+        Swal.fire({
+            title: 'Êtes-vous sûr de vouloir supprimer ce Offrande ?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Oui, supprimer',
+            cancelButtonText: 'Non, annuler'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios
+                    .delete(`${url}typeoffrandes/${id}`, {
+                        headers: {
+                            Accept: 'application/json',
+                            'Content-Type': 'application/json',
+                            Authorization: Token()
+                        }
+                    })
+                    .then((response) => {
+                        dispatch({ type: DELETE_OFFRANDE, payload: id });
+                        Swal.fire({
+                            icon: 'success',
+                            text: `${response.data.message}`,
+                            confirmButtonText: 'OK'
+                        });
+                    })
+                    .catch((error) => {
+                        alert(error);
+                    });
+            }
+        });
+    };
+};
