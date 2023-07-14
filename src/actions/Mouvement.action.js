db319483abdd0f768a2a4c418eda5d1e1fee1658
verstands <rabbymbamu@gmail.com>
2023-07-14 11:01:02 +0100

    delete action mouvement

--- src/actions/Mouvement.action.js
+++ src/actions/Mouvement.action.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import Token from "../loadingErr/Token";
 
 export const GET_MOUVEMENT = "GET_MOUVEMENT";
+export const DELETE_MOUVEMENT = "DELETE_MOUVEMENT";
 const url = "http://localhost:5000/api/";
 
 
@@ -20,3 +21,37 @@ export const getMouvement = () => {
             })
     }
 }
+
+export const deleteMouvement = (id) => {
+    return (dispatch) => {
+        Swal.fire({
+            title: 'Êtes-vous sûr de vouloir supprimer ce Categorie ?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Oui, supprimer',
+            cancelButtonText: 'Non, annuler'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios
+                    .delete(`${url}mouvement/${id}`, {
+                        headers: {
+                            Accept: 'application/json',
+                            'Content-Type': 'application/json',
+                            Authorization: Token()
+                        }
+                    })
+                    .then((response) => {
+                        dispatch({ type: DELETE_MOUVEMENT, payload: id });
+                        Swal.fire({
+                            icon: 'success',
+                            text: `${response.data.message}`,
+                            confirmButtonText: 'OK'
+                        });
+                    })
+                    .catch((error) => {
+                        alert(error);
+                    });
+            }
+        });
+    };
+};
