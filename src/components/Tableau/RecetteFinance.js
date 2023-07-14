8b57bacd256f848bf76be70313ae9199348c9983
verstands <rabbymbamu@gmail.com>
2023-07-14 12:14:59 +0100

    tableau recette

--- src/components/Tableau/RecetteFinance.js
+++ src/components/Tableau/RecetteFinance.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
 import { Link } from "react-router-dom";
+import { deleteTypeDepense } from '../../actions/TypeDepenseAction';
 
 
-function RecetteFinance({nfiche, culte_id,montant, typeoffrance_id, devise, homme, femme, enfant}) {
+function RecetteFinance({nfiche, culte_id,montant,id, typeoffrance_id, devise, homme, femme, enfant}) {
+    const dispatch = useDispatch();
     return (
         <>
             <tr>
@@ -10,7 +13,7 @@ function RecetteFinance({nfiche, culte_id,montant, typeoffrance_id, devise, homm
                 <td class="d-none d-xl-table-cell">{montant}</td>
                 <td class="d-none d-xl-table-cell">{devise && devise.devise}</td>
                 <td class="d-none d-md-table-cell">
-                    <button class="badge bg-danger btn"><span class="badge bg-danger">Delete</span></button>&nbsp;
+                    <button class="badge bg-danger btn" onClick={() => dispatch(deleteTypeDepense(id))}><span class="badge bg-danger">Delete</span></button>&nbsp;
                     <Link to="/updatemembre" class="badge bg-success btn"><span class="badge bg-success">Update</span></Link>&nbsp;
                 </td>
             </tr>
