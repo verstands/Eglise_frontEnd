6e85207198055881b260d68968fb9e80cfe44760
verstands <rabbymbamu@gmail.com>
2023-07-14 12:33:36 +0100

    Reducer enfant

--- src/Reducers/DeviseReducer.js
+++ src/Reducers/DeviseReducer.js
@@ -3,12 +3,12 @@ import { GET_DEVISE, DELETE_DEVISE } from "../actions/DeviseAction"
 
 const initiatState = {}
 
-export default function deviseReducer(state = initiatState, action){
-   switch(action.type){
+export default function deviseReducer(state = initiatState, action) {
+   switch (action.type) {
       case GET_DEVISE:
          return action.payload
-         case DELETE_DEVISE:
-            return state.filter((post) => post.id != action.payload);
+      case DELETE_DEVISE:
+         return state.filter((post) => post.id != action.payload);
       default:
          return state
    }
--- src/Reducers/Enfant.reducer.js
+++ src/Reducers/Enfant.reducer.js
@@ -1,12 +1,14 @@
-import { GET_MEMBRE_ENFANT } from "../actions/Enfant.action";
+import { GET_MEMBRE_ENFANT, DELETE_MEMBRE_ENFANT } from "../actions/Enfant.action";
 
 
 const initiatState = {}
 
-export default function enfantReducer(state = initiatState, action){
-   switch(action.type){
+export default function enfantReducer(state = initiatState, action) {
+   switch (action.type) {
       case GET_MEMBRE_ENFANT:
          return action.payload
+      case DELETE_DEVISE:
+         return state.filter((post) => post.id != action.payload);
       default:
          return state
    }
