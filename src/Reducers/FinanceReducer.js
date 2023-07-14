2d5f65bbb5ddcbb7f277fba34e2a4a4ee21ba814
verstands <rabbymbamu@gmail.com>
2023-07-14 12:34:15 +0100

    Reducer finance

--- src/Reducers/FinanceReducer.js
+++ src/Reducers/FinanceReducer.js
@@ -1,12 +1,14 @@
-import { GET_FINANCE } from "../actions/FinanceAction"
+import { GET_FINANCE, DELETE_FINANCE } from "../actions/FinanceAction"
 
 
 const initiatState = {}
 
-export default function financeReducer(state = initiatState, action){
-   switch(action.type){
+export default function financeReducer(state = initiatState, action) {
+   switch (action.type) {
       case GET_FINANCE:
          return action.payload
+      case DELETE_MEMBRE_ENFANT:
+         return state.filter((post) => post.id != action.payload);
       default:
          return state
    }
