fb118c5ab42bd12748f9becc3222443af3e6980f
verstands <rabbymbamu@gmail.com>
2023-07-25 13:25:56 +0100

    tableau materiel

--- src/components/Tableau/MaterielTableau.js
+++ src/components/Tableau/MaterielTableau.js
@@ -3,7 +3,7 @@ import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { deleteMateriel } from '../../actions/MaterielAction'
 
-const MaterielTableau = ({materiel,id, categorie_id, stock, cout, devide_id, id_departement}) => {
+const MaterielTableau = ({ materiel, id, categorie_id, stock, cout, devide_id, id_departement }) => {
     const dispatch = useDispatch();
     return (
         <>
@@ -15,10 +15,14 @@ const MaterielTableau = ({materiel,id, categorie_id, stock, cout, devide_id, id_
                 <td class="d-none d-xl-table-cell">{devide_id}</td>
                 <td class="d-none d-xl-table-cell">{id_departement}</td>
                 <td class="d-none d-md-table-cell">
-                    <button class="badge bg-danger btn" onClick={() => dispatch(deleteMateriel(id))}>
-                        <span class="badge bg-danger">Delete</span>
+                    <button className="btn-lg bg-danger btn" onClick={() => dispatch(deleteMateriel(id))}>
+                        <span class="badge bg-danger">
+                            <i className="fa fa-trash" style={{ color: 'white' }}></i>
+                        </span>
                     </button>&nbsp;
-                    <Link to="/updatemembre" class="badge bg-success btn"><span class="badge bg-success">Update</span></Link>&nbsp;
+                    <Link to="/updatemembre"  className="btn-lg bg-success btn"><span class="badge bg-success">
+                        <i className="fa fa-edit" style={{ color: 'white' }}></i>
+                    </span></Link>&nbsp;
                 </td>
             </tr>
         </>
