73e193b96d0a899226d749f32fa7036c61aa8156
verstands <rabbymbamu@gmail.com>
2023-07-25 14:02:21 +0100

    fin materiel

---
+++ src/components/Materiel/MaterielPdf.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MaterielPdf() {
+  return (
+    <div>MaterielPdf</div>
+  )
+}
+
+export default MaterielPdf
--- src/components/Tableau/MaterielTableau.js
+++ src/components/Tableau/MaterielTableau.js
@@ -9,11 +9,11 @@ const MaterielTableau = ({ materiel, id, categorie_id, stock, cout, devide_id, i
         <>
             <tr>
                 <td class="d-none d-xl-table-cell">{materiel}</td>
-                <td class="d-none d-xl-table-cell">{categorie_id}</td>
+                <td class="d-none d-xl-table-cell">{categorie_id && categorie_id.nom}</td>
                 <td class="d-none d-xl-table-cell">{stock}</td>
                 <td class="d-none d-xl-table-cell">{cout}</td>
-                <td class="d-none d-xl-table-cell">{devide_id}</td>
-                <td class="d-none d-xl-table-cell">{id_departement}</td>
+                <td class="d-none d-xl-table-cell">{devide_id && devide_id.devise}</td>
+                <td class="d-none d-xl-table-cell">{id_departement && id_departement.nom_depart}</td>
                 <td class="d-none d-md-table-cell">
                     <button className="btn-lg bg-danger btn" onClick={() => dispatch(deleteMateriel(id))}>
                         <span class="badge bg-danger">
