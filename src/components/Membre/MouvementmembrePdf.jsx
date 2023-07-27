ea2a347e756db9cdaede86fe7df61f1bd99a9332
verstands <rabbymbamu@gmail.com>
2023-07-27 03:30:56 +0200

    de

--- src/components/Membre/MouvementmembrePdf.jsx
+++ src/components/Membre/MouvementmembrePdf.jsx
@@ -46,8 +46,8 @@ const styles = StyleSheet.create({
     },
     table: { display: "table", width: "auto", borderStyle: "solid", borderColor: "#bfbfbf", borderWidth: 1, borderRightWidth: 0, borderBottomWidth: 0 },
     tableRow: { margin: "auto", flexDirection: "row", fontSize: 1 },
-    tableColHeader: { width: "9%", borderStyle: "solid", borderColor: "#bfbfbf", borderBottomColor: "#000", borderWidth: 1, backgroundColor: "#f0f0f0" },
-    tableCol: { width: "9%", borderStyle: "solid", borderColor: "#bfbfbf", borderBottomWidth: 1 },
+    tableColHeader: { width: "25%", borderStyle: "solid", borderColor: "#bfbfbf", borderBottomColor: "#000", borderWidth: 1, backgroundColor: "#f0f0f0" },
+    tableCol: { width: "25%", borderStyle: "solid", borderColor: "#bfbfbf", borderBottomWidth: 1 },
     tableCellHeader: { margin: 4, fontSize: 8, fontWeight: 'bold' },
     tableCell: { margin: 1, fontSize: 9 },
     tableRow2: { margin: "auto", flexDirection: "row", fontSize: 1 },
@@ -74,7 +74,6 @@ function MouvementmembrePdf() {
                                 <Text>Image                                                                           </Text>
                             </div>
                             <div className="col-md-6">
-                                <Text style={{ fontSize: 10 }}>LOGinSmart</Text>
                                 <Text style={{ fontSize: 10 }}>Kinshasa/Gombe</Text>
                                 <Text style={{ fontSize: 10 }}>Republique Democratique du Congo</Text>
                             </div>
@@ -82,7 +81,7 @@ function MouvementmembrePdf() {
                     </View>
                     <View style={styles.Titre}>
                         <div className="text-center">
-                            <Text style={{ fontSize: 10, fontWeight: "bold" }}>RESULTATS DE RECHERCHE POUR PRELEVEMENTS VEHICULES</Text>
+                            <Text style={{ fontSize: 10, fontWeight: "bold" }}>LISTE DES TOUT LES MOUVEMENTS</Text>
                         </div>
                     </View>
                     <View style={styles.body}>
@@ -104,6 +103,9 @@ function MouvementmembrePdf() {
                                     <View style={styles.tableColHeader}>
                                         <Text style={styles.tableCellHeader}>Mission</Text>
                                     </View>
+                                    <View style={styles.tableColHeader}>
+                                        <Text style={styles.tableCellHeader}>Date</Text>
+                                    </View>
                                 </View>
                                 {Array.isArray(carss) && carss.map((item) => (
                                     <>
@@ -112,10 +114,13 @@ function MouvementmembrePdf() {
                                                 <Text style={styles.tableCell}>{n++}</Text>
                                             </View>
                                             <View style={styles.tableCol}>
-                                                <Text style={styles.tableCell}>{item.nom_membrem}</Text>
+                                                <Text style={styles.tableCell}>{item.membre && item.membre.nom}</Text>
                                             </View>
                                             <View style={styles.tableCol}>
-                                                <Text style={styles.tableCell}>{item.departement_id}</Text>
+                                                <Text style={styles.tableCell}>{item.mission && item.mission.mission}</Text>
+                                            </View>
+                                            <View style={styles.tableCol}>
+                                                <Text style={styles.tableCell}>{dateFormat(item.created_at, 'dd/mm/yyyy')}</Text>
                                             </View>
                                         </View>
                                     </>
